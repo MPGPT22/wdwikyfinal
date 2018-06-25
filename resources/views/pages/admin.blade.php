d6d3f381db661ac4661aa498033f72f561b1f11d
unknown <joao_MPGPT21@hotmail.com>
2018-06-25 13:38:09 +0100

    Almost Finished

--- resources/views/pages/admin.blade.php
+++ resources/views/pages/admin.blade.php
@@ -14,18 +14,16 @@
 @endsection
 
 @section('content')
-        <div class="col-md-10">
+        <div class="col-md-12">
             <div class="panel panel-default">
-                <div class="panel-heading">Admin Dashboard</div>
-
                 <div class="panel-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in as Admin!
+                  @if (Auth::user()->adminRole == "GameMaster")
+                  <h4 class="title">You are the GameMaster, you have the power to do anything you like, from creating to deleting and to controll any commentary.</h4>
+                  @elseif (Auth::user()->adminRole == "Creator")
+                  <h4 class="title">You are the Creator, you have the power to create anything but you can't controll the commentaries of your creations</h4>
+                  @elseif (Auth::user()->adminRole == "Comment Checker")
+                  @endif
+                    
                 </div>
             </div>
         </div>
